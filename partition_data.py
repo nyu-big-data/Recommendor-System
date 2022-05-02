57f906b11d7ca6ac24f6cf42c364b9ebebaa4880
garimagupta85 <garimag8599@gmail.com>
2022-05-01 21:34:10 -0400

    Updated code

--- partition_data.py
+++ partition_data.py
@@ -6,6 +6,9 @@ from pyspark.sql import functions as F
 from pyspark.sql import DataFrameStatFunctions as statFunc
 from pyspark.sql import Window
 from pyspark.sql.functions import col
+from pyspark.sql.functions import countDistinct
+from functools import reduce
+from pyspark.sql import DataFrame
 
 
 def main(spark, netID):
@@ -20,14 +23,21 @@ def main(spark, netID):
     ratings = spark.read.csv('ratings.csv', schema='userId INTEGER, movieId INTEGER, rating DOUBLE, timestamp INTEGER')
     ratings.printSchema()
 
-    c = int(ratings.count() // 2)
-    temp_df = ratings.sort('userId').limit(c)
-    temp_df2 = ratings.sort('userId', ascending=False).limit(c)
+    temp = ratings.filter((col('userId') <= 478) & (col('userId') >= 153))
+    temp_df = ratings.filter(col('userId') > 478)
+    temp_df2 = ratings.filter(col('userId') < 153)
 
-    # temp_df = temp_df.groupby('userId').orderby('timestamp')
-    # temp_df2 = temp_df2.groupby('userId').orderby('timestamp')
-    n = 8
+    # # temp_df = temp_df.groupby('userId').orderby('timestamp')
+    # # temp_df2 = temp_df2.groupby('userId').orderby('timestamp')
+    
+    # temp_df.show(10)
+    # temp_df.tail(10) 
+       
+    # temp_df2.show(10)
+    # temp_df2.tail(10) 
 
+    n = 15
+       
     window_1 = Window.partitionBy(temp_df['userId']).orderBy('timestamp')
     train_1 = temp_df.select('*', F.rank().over(window_1).alias('rank')).filter(F.col('rank') > n).drop('rank')
     test = temp_df.select('*', F.rank().over(window_1).alias('rank')).filter(F.col('rank') <= n).drop('rank')
@@ -36,12 +46,18 @@ def main(spark, netID):
     train_2 = temp_df2.select('*', F.rank().over(window_2).alias('rank')).filter(F.col('rank') <= n).drop('rank')
     valid = temp_df2.select('*', F.rank().over(window_2).alias('rank')).filter(F.col('rank') > n).drop('rank')
 
-    train = train_1.union(train_2)
+    dfs = [train_1, train_2, temp]
+
+    # create merged dataframe
+    train = reduce(DataFrame.unionAll, dfs)
+    train = train.orderBy('userId')
+    test = test.orderBy('userId')
+    valid = valid.orderBy('userId')
 
     #Saving to csv 
-    train .coalesce(1).write.csv('hdfs:/user/{}/train-small.csv'.format(netID))
-    valid.coalesce(1).write.csv('hdfs:/user/{}/valid-small.csv'.format(netID))
-    test.coalesce(1).write.csv('hdfs:/user/{}/test-small.csv'.format(netID))
+    train .coalesce(1).write.csv('hdfs:/user/{}/train-small-2.csv'.format(netID))
+    valid.coalesce(1).write.csv('hdfs:/user/{}/valid-small-2.csv'.format(netID))
+    test.coalesce(1).write.csv('hdfs:/user/{}/test-small-2.csv'.format(netID))
 
 # Only enter this block if we're in main
 if __name__ == "__main__":
