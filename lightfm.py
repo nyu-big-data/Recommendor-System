593150069b3c0f6462246fe84c22e7c8c1544d52
garimagupta85 <garimag8599@gmail.com>
2022-05-17 19:48:17 -0400

    evalution added

--- lightfm.py
+++ lightfm.py
@@ -2,17 +2,31 @@ import pandas as pd
 import numpy as np
 import scipy.sparse as sp
 from lightfm import LightFM
-from lightfm.evaluation import precision_at_k
+from lightfm.data import Dataset
+from lightfm.evaluation import precision_at_k, auc_score
 
 import time 
 
-train = pd.read_csv('/content/trainsmall.csv', names= ['userId', 'movieId', 'rating', 'timestamp'])
-users = train["userId"].values
-products = train["movieId"].values
-ratings = train["rating"].values
-train = sp.coo_matrix((ratings, (users, products)))
-# train.head()
-# Instantiate and train the model
+data = pd.read_csv('/content/ratings.csv', names= ['userId', 'movieId', 'rating', 'timestamp'])
+train = pd.read_csv('/content/train.csv', names= ['userId', 'movieId', 'rating', 'timestamp'])
+train = train.drop(['timestamp'], axis = 1)
+test = pd.read_csv('/content/test.csv', names= ['userId', 'movieId', 'rating', 'timestamp'])
+test = test.drop(['timestamp'], axis = 1)
+
+user_id = data['userId'].unique()[1:].astype('int64')
+movie_id = data['movieId'].unique()[1:].astype('int64')
+
+data = Dataset()
+
+data.fit(user_id,  
+         movie_id 
+        )
+
+train_values = train.values[1:].astype('int64')
+test_values = test.values[1:].astype('int64')
+
+train, weights_matrix = data.build_interactions([tuple(i) for i in train_values])
+test, weights_matrix = data.build_interactions([tuple(i) for i in test_values])
 
 start_time = time.time()
 model = LightFM(loss='warp')
@@ -21,11 +35,13 @@ end_time = time.time()
 
 print("Total LightFM model training time: {} seconds".format(end_time - start_time))
 
-# test = pd.read_csv('/content/testsmall.csv', names= ['userId', 'movieId', 'rating', 'timestamp'])
-# users = test["userId"].values
-# products = test["movieId"].values
-# ratings = test["rating"].values
-# test = sp.coo_matrix((ratings, (users, products)))
+training_auc = auc_score(model, train).mean()
+
+print("Training Accuracy : {}".format(training_auc))
 
 # Evaluate the trained model
-# test_precision = precision_at_k(model, test, k=5).mean()
+test_precision = precision_at_k(model, test, k=100).mean()
+test_auc = auc_score(model, test).mean()
+
+print("Testing Precision: {}".format(test_precision))
+print("Testing Accuracy: {}".format(test_auc))
